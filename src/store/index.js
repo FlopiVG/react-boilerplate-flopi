5093d5099df9c156b7d7113de54849dae92c5768
Flopi <flopivillagarcia@gmail.com>
2017-10-13 21:54:18 +0200

    add redux dev tool and currently in node scripts

--- src/store/index.js
+++ src/store/index.js
@@ -5,4 +5,9 @@ import reducers from 'reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
-export default createStoreWithMiddleware(reducers);
+export default createStoreWithMiddleware(
+  reducers,
+  /* eslint-disable no-underscore-dangle */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  /* eslint-enable */
+);
